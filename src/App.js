96bee8a7128310310d9a319099d4549958de479c
John Clowers <jclowers18@gmail.com>
2023-08-17 17:07:30 -0500

    Fixed and functional

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
 import Banner from "./components/Banner";
-import useState from "react";
+import { useState } from "react";
 import satData from "./components/satData";
 
 function App() {
-    const [sat, setSat] = useState(satData);
-    const displaySats = [...new Set(satData.map((data) => data.orbitType))];
-    
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+  const filterByType = (currentType) => {
+    const displaySats = satData.filter((newSatDisplay) => {
+      return newSatDisplay.orbitType === currentType;
+    });
+    setSat(displaySats);
+  };
   return (
     <div>
       <Banner />
       <Buttons 
-           filterByType={filterByType}
-           setSat={setSat}
-           displaySats={displaySats}
-           />
-      <Table sat={sat} />
+        filterByType={filterByType}
+        setSat={setSat}
+        displaySats={displaySats}
+      />
+      <Table sat={sat}/>
     </div>
   );
 }
-const filterByType = (currentType) => {
-  const newSat = satData.filter((newSatDisplay) => {
-     return newSatDisplay.orbitType === currentType;
-  });
-  setSat(newSat);
- 
-};
 
 
 export default App;
